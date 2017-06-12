cc2cd36ba27289fc0c14ecd0a763d6e3d582e380
zaworka <jesch@mail.de>
2017-06-12 17:07:41 +0200

    added "sequence"- and "Word"-Classes and parser-function; reworked spacedWords-function

--- main.cpp
+++ main.cpp
@@ -1,127 +1,189 @@
 #include <iostream>
-#include <string>
-#include <bitset> // output binary
-#include <map>
-#include <list>
-#include <iterator> // advance(iterator, int)
-
+#include <fstream>
+#include <vector>
+#include <time.h>
+#include <algorithm>
+#include <tuple>
 using namespace std;
 
-list<long long> spacedWords (string const& seq, string const& pattern)
+class Word
 {
-    list<long long> out;
-    long long word = 0;
-    long long tmp;
-    for (unsigned int i = 0; i < (seq.length() - pattern.length() + 1); ++i)
+    public:
+        unsigned long long key;
+        unsigned int pos;
+        void set_key (unsigned long long);
+        void set_pos (unsigned int);
+        bool operator< (const Word& w) const { return key < w.key; }
+};
+
+void Word::set_key (unsigned long long x)
+{
+    key = x;
+}
+
+void Word::set_pos (unsigned int x)
+{
+    pos = x;
+}
+
+class Sequence
+{
+    public:
+        string header;
+        vector<char> seq;
+        vector<Word> sorted_words;
+        void set_header (string);
+        void clear_header ();
+        void set_seq (char);
+        void clear_seq ();
+        void set_words (vector<Word>);
+};
+
+void Sequence::set_header(string x)
+{
+    header = x;
+}
+
+void Sequence::clear_header()
+{
+    header.clear();
+}
+
+void Sequence::set_seq(char x)
+{
+    seq.push_back(x);
+}
+
+void Sequence::clear_seq()
+{
+    seq.clear();
+}
+
+void Sequence::set_words(vector<Word> x)
+{
+    sort(x.begin(), x.end() );
+    sorted_words = x;
+}
+
+vector<Sequence> parser (int argc, char **argv, vector<Sequence>& out)
+{
+    if (argc <= 1)
     {
-        for (unsigned int j = 0; j < pattern.length(); j++)
+        cerr << "Usage: " << argv[0] << " [infile]\n";
+    }
+
+    ifstream input(argv[1]);
+    if (!input.good() )
+    {
+        cerr << "Error opening '" << argv[1] << "'. Bailing out.\n";
+    }
+    Sequence tmp;
+    string line;
+    while (getline(input,line).good() )
+    {
+        if (line.empty() || line[0] == '>')
         {
-            if (pattern[j] == '1') {
-                switch (seq[i]) {
-                    case 'A': tmp = 1; break;
-                    case 'B': tmp = 2; break;
-                    case 'C': tmp = 3; break;
-                    case 'D': tmp = 4; break;
-                    case 'E': tmp = 5; break;
-                    case 'F': tmp = 6; break;
-                    case 'G': tmp = 7; break;
-                    case 'H': tmp = 8; break;
-                    case 'I': tmp = 9; break;
-                    case 'K': tmp = 10; break;
-                    case 'L': tmp = 11; break;
-                    case 'M': tmp = 12; break;
-                    case 'N': tmp = 13; break;
-                    case 'P': tmp = 14; break;
-                    case 'Q': tmp = 15; break;
-                    case 'R': tmp = 16; break;
-                    case 'S': tmp = 17; break;
-                    case 'T': tmp = 18; break;
-                    case 'V': tmp = 19; break;
-                    case 'W': tmp = 20; break;
-                    case 'X': tmp = 21; break;
-                    case 'Y': tmp = 22; break;
-                    case 'Z': tmp = 23; break;
-                    case '*': tmp = 24; break;
+            if (!tmp.header.empty() )
+            {
+                out.push_back(tmp);
+                tmp.clear_header();
+                tmp.clear_seq();
+            }
+            if (!line.empty() )
+            {
+                tmp.set_header(line.substr(1) );
+            }
+            tmp.clear_seq();
+        }
+        else if (!tmp.header.empty() )
+        {
+
+            for (unsigned int i = 0; i < line.size(); ++i)
+            {
+                switch (line[i]) {
+                    case 'A': tmp.set_seq(1); break;
+                    case 'B': tmp.set_seq(2); break;
+                    case 'C': tmp.set_seq(3); break;
+                    case 'D': tmp.set_seq(4); break;
+                    case 'E': tmp.set_seq(5); break;
+                    case 'F': tmp.set_seq(6); break;
+                    case 'G': tmp.set_seq(7); break;
+                    case 'H': tmp.set_seq(8); break;
+                    case 'I': tmp.set_seq(9); break;
+                    case 'K': tmp.set_seq(10); break;
+                    case 'L': tmp.set_seq(11); break;
+                    case 'M': tmp.set_seq(12); break;
+                    case 'N': tmp.set_seq(13); break;
+                    case 'P': tmp.set_seq(14); break;
+                    case 'Q': tmp.set_seq(15); break;
+                    case 'R': tmp.set_seq(16); break;
+                    case 'S': tmp.set_seq(17); break;
+                    case 'T': tmp.set_seq(18); break;
+                    case 'V': tmp.set_seq(19); break;
+                    case 'W': tmp.set_seq(20); break;
+                    case 'X': tmp.set_seq(21); break;
+                    case 'Y': tmp.set_seq(22); break;
+                    case 'Z': tmp.set_seq(23); break;
+                    case '*': tmp.set_seq(24); break;
                 }
-                word <<= 5;
-                word |= tmp;
+            }
+        }
+    }
+    return out;
+}
+
+void spacedWords (Sequence& sequence, string const& pattern, vector<Word>& out)
+{
+    for (unsigned int i = 0; i < (sequence.seq.size() - pattern.length() + 1); ++i)
+    {
+        Word tmp;
+        unsigned long long spacedWord = 0;
+        for (unsigned int j = 0; j < pattern.size(); ++j)
+        {
+            if (pattern[j] == '1')
+            {
+                spacedWord <<= 5;
+                spacedWord |= sequence.seq[i];
             }
             i++;
         }
         i = i - pattern.length();
-        out.push_back(word);
-        word = 0;
-     }
-     out.sort();
-     return out;
+        tmp.set_key(spacedWord);
+        tmp.set_pos(i);
+        out.push_back(tmp);
+    }
+    sequence.set_words(out);
 }
 
-list <long long> findMatches (list<long long> const &sw1, list<long long>& sw2)
+int main(int argc, char **argv)
 {
-    int skip = 0;
-    list <long long> out;
-    for (list<long long>::const_iterator it = sw1.begin(); it != sw1.end(); it++)
+    clock_t t;
+    t = clock();
+
+    string pattern = argv[2];
+
+    vector<Sequence> sequences;
+    parser(argc, argv, sequences);
+    for (unsigned int i = 0; i < sequences.size(); ++i)
     {
-        for (list<long long>::iterator it2 = sw2.begin(); it2 != sw2.end(); it2++)
+        vector<Word> sw;
+        spacedWords(sequences[i], pattern, sw);
+    }
+
+    /* TEST */
+    for (unsigned int i = 0; i < sequences.size(); ++i)
+    {
+        cout << sequences[i].header << endl << "Found Spaced-Words:\n";
+        for (unsigned int j = 0; j < sequences[i].sorted_words.size(); ++j)
         {
-            advance(it2,skip);
-            if (*it > *it2)
-            {
-                skip++;
-                continue;
-            }
-            if (*it < *it2)
-            {
-                break;
-            }
-            if (*it == *it2)
-            {
-                skip++;
-                out.push_back(*it);
-            }
+            cout <<  sequences[i].sorted_words[j].key << " | Pos: " << sequences[i].sorted_words[j].pos << endl;
         }
+        cout << endl;
     }
-    out.sort();
-    return out;
-}
+    /* TEST-ENDE */
 
-int main()
-{
-    /*Required variables*/
-    string sequence1;
-    string sequence2;
-    string pattern;
-
-
-    cout << "Enter a protein sequence\n";
-    cin >> sequence1;
-    cout << "Enter another protein sequence\n";
-    cin >> sequence2;
-    cout << "Enter a pattern with no more than 12 Care-Positions\n";
-    cin >> pattern;
-
-    list<long long> spaced_words1 = spacedWords(sequence1, pattern);
-    cout << "First SW-List:\n";
-    for(list<long long>::iterator it = spaced_words1.begin(); it != spaced_words1.end(); it++)
-    {
-        cout << *it << endl;
-    }
-
-    list<long long> spaced_words2 = spacedWords(sequence2, pattern);
-    cout << "Second SW-List:\n";
-    for(list<long long>::iterator it = spaced_words2.begin(); it != spaced_words2.end(); it++)
-    {
-        cout << *it << endl;
-    }
-
-    list<long long> matches = findMatches(spaced_words1, spaced_words2);
-    cout << "Matches found:\n";
-    for(list<long long>::iterator it = matches.begin(); it != matches.end(); it++)
-    {
-        cout << *it << endl;
-    }
-
-    cin.get();
+    t = clock() - t;
+    cout << "Time elapsed: " << t*1.0/CLOCKS_PER_SEC << "s\n";
+    //cin.get();
     return 0;
 }
-
